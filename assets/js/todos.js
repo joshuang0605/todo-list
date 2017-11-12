efbe3f0144fbf78d6df45dac5bdc755ae83558d3
Long <joshua.ng0605@gmail.com>
2017-11-13 12:29:36 +1300

    Method 1 to apply click function changing color

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,7 +1,23 @@
-$(.li).click(function(){
-  $(this).css {
-    color: "yellow";
-  }
-});
+// Check off specific todos by clicking
+// Select all LI and put the CLICK event when being clicked
 
-alert("1");
+/*Method 1 to apply click function
+$("li").click(function(){
+  //if li is gray
+   if($(this).css("color") === "rgb(128, 128, 128)"){
+     //turn it black
+     $(this).css({
+       color: "black",
+       textDecoration: "none"
+     });
+  //else turn it gray and line-through
+   } else {
+  $(this).css({
+    color: "gray",
+    textDecoration: "line-through"
+     });
+   }
+});
+*/
+
+//Mehod 2
